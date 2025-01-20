179488abc53494da9066ff9fec03474fea76651f
piyush-panpaliya <panpaliyapiyush0@gmail.com>
2025-01-20 16:37:12 +0530

    footer

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -10,12 +10,19 @@ export function meta({}: Route.MetaArgs) {
 
 export default function Home() {
   return (
-    <main className='container mx-auto p-4'>
+    <main className='container mx-auto p-4 flex flex-col min-h-svh'>
       <h1 className='text-3xl font-bold mb-4'>
         Personalized Timetable Generator
       </h1>
       <div className='hidden' />
-      <TimetableGenerator />
+      <div className='w-full grow'>
+        <TimetableGenerator />
+      </div>
+      <footer className='mt-8 text-center hover:underline underline-offset-2 text-gray-700'>
+        <a href='https://github.com/piyush-panpaliya/time-table'>
+          made with ❤️ by PSH
+        </a>
+      </footer>
     </main>
   )
 }
